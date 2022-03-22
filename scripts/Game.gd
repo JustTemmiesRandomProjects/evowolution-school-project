cbd388d4e22ec303ec90379a247e00a85b979750
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-03-22 16:50:54 +0100

    beavr

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,6 +1,26 @@
 extends Node2D
 
-
+const top_fence = preload("res://assets/top_fence.png")
+const bottom_fence = preload("res://assets/bottom_fence.png")
+const fence_gate = preload("res://assets/fence.png")
+const bomb_display = preload("res://assets/bombsdisplay.png")
 
 func _on_GameTick_timeout() -> void:
 	$CanvasLayer/Label.text = "Coins: " + str(Stats.coins)
+
+func _input(event):
+	if InputEventMouseMotion:
+		if Stats.hotbar == 3:
+			$Fence.texture = bottom_fence
+		elif Stats.hotbar == 4:
+			$Fence.texture = top_fence
+		elif Stats.hotbar == 5:
+			$Fence.texture = fence_gate
+		elif Stats.hotbar == 6:
+			$Fence.texture = bomb_display
+		else:
+			$Fence.hide()
+		
+		if Stats.hotbar == 3 or Stats.hotbar == 4 or Stats.hotbar == 5 or Stats.hotbar == 6:
+			$Fence.show()
+			$Fence.position = get_global_mouse_position()-Vector2(512,300)
--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -10,16 +10,7 @@ extends StaticBody2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-func _process(delta: float) -> void:
-	if $Sprite.timer >= 48:
-		$Sprite2.scale += Vector2(25*delta,25*delta)
-		if $Sprite2.R > 1.05:
-			$Sprite2.R -= 3*delta
-		if $Sprite2.R < 0.95:
-			$Sprite2.R += 3*delta
-
-
 func _on_Sprite_boom() -> void:
 	for x in $Area2D.get_overlapping_bodies():
 		x.queue_free()
+	queue_free()
